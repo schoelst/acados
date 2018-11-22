29b3c3ffd20c81e491edc8118c2aa9ab133d6dc6
Tobias Schoels <tobias.schoels@gmail.com>
2018-11-22 14:15:16 +0100

    add current state of integrator_test.py

---
+++ examples/python/integrator_test.py
@@ -0,0 +1,82 @@
+from acados import *
+import time
+import numpy as np
+from models import pendulum_model, chen_model
+
+# get model
+ode_fun, nx, nu, impl_ode_fun = pendulum_model()
+
+# define initial value, controls
+x0 = np.array([4.2, 0.42, 0, 0])
+u = np.array([1])
+
+# create integrator
+opts2 = {'step_size': 0.01}
+start = time.time()
+sim2 = integrator(ode_fun, opts2)
+stop = time.time()
+print("time to create: " + str(stop-start))
+# call integrator without argument
+out_ = sim2.integrate()
+print("integrator result: " + str(out_))
+
+# call integrator with argument
+# in_ = {'x0': x0, 'u': u}
+# out_ = sim2.integrate(in_)
+print("integrator result: " + str(out_))
+input("press any key to continue")
+
+print("sim2 settings")
+sim2.print_settings()
+
+# input("press any key to continue")
+
+
+# create another integrator
+opts1 = {'step_size': 0.01,  # only mandatory argument
+         # OPTIONAL ARGUMENTS
+         'model_type': 1,  # 0 - EXPLICIT (default)
+         # 1 - IMPLICIT
+         'integrator': "IRK",  # default (ERK)
+         'use_MX': True,  # default (False)
+         'ns': 3,  # default set in C implementation of integrator used
+         'num_steps': 1,  # default set in C implementation of integrator used
+         'newton_iter': 3,  # default set in C implementation of integrator used
+         'output_z': 0,  # default set in C implementation of integrator used
+         'sens_forw': 1,  # default set in C implementation of integrator used
+         'sens_adj': 1,  # default set in C implementation of integrator used
+         'sens_hess': 1,  # default set in C implementation of integrator used
+         'sens_algebraic': 0,  # default set in C implementation of integrator used
+         'jac_reuse': 0,  # default set in C implementation of integrator used
+         }
+
+sim1 = integrator(impl_ode_fun, opts1)
+sim1.print_settings()
+
+# set experiment parameters
+M = 6
+x1 = x0
+x2 = x0
+start_time = 0.0
+t1 = 0.0
+t2 = 0.0
+
+# run experiment
+for k in range(M):
+    start_time = time.time()    # start timer
+    print("1st sim1 call")
+    x1 = np.array(sim1.integrate(x1, u))
+    t1 += time.time() - start_time
+
+    start_time = time.time()    # start timer
+    x2 = np.array(sim2.integrate(x2, u))
+    t2 += time.time() - start_time
+
+#     sim1.set_step(sim1.step()*0.99)
+    # step can be updated
+    print("")
+    print(x1)
+    print(x2)
+
+print("avg time IRK: ", t1/M)
+print("avg time ERK: ", t2/M)

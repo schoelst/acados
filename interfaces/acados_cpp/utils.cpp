e92cbbfe8fc0d3b770bc8a2e110540d57ab3f406
Tobias Schoels <tobias.schoels@gmail.com>
2018-11-27 14:33:37 +0100

    move conversion functions to header (required)

--- interfaces/acados_cpp/utils.cpp
+++ interfaces/acados_cpp/utils.cpp
@@ -41,43 +41,4 @@ const T& clamp(const T& lo, const T& hi, const T& val)
     return val;
 }
 
-template <typename T>
-vector<T> matrix2columnmajor(vector<vector<T>> matrix)
-{
-    vector<T> colvec;
-    if (matrix.size() == 0) return colvec;
-
-    const size_t n = matrix.size();
-    const size_t m = matrix[0].size();
-    colvec.reserve(n * m);
-    for (auto const& row : matrix)
-    {
-        if (row.size() != m) throw std::length_error("matrix has inconsistent dimensions");
-        colvec.insert(colvec.end(), row.begin(), row.end());
-    }
-    return colvec;
-}
-
-template <typename T>
-vector<vector<T>> columnmajor2matrix(vector<T> colvec, const size_t n, const size_t m)
-{
-    vector<vector<T>> matrix;
-    if (colvec.size() == 0) return matrix;
-
-    if (colvec.size() != n * m) throw std::length_error("colvec has inconsistent size");
-
-
-    matrix.reserve(n);
-    auto start = colvec.begin();
-    while (start != colvec.end())
-    {
-        matrix.push_back(vector<T>(start, start + m));
-        start = start + m + 1;  // element after end
-    }
-    if (matrix.size() != n)
-        throw std::length_error("conversion failed: output matrix as wrong dimensions");
-
-    return matrix;
-}
-
 }  // namespace acados
--- interfaces/acados_cpp/utils.hpp
+++ interfaces/acados_cpp/utils.hpp
@@ -2,6 +2,7 @@
 #ifndef INTERFACES_ACADOS_CPP_UTILS_HPP_
 #define INTERFACES_ACADOS_CPP_UTILS_HPP_
 
+#include <stdexcept>
 #include <string>
 #include <utility>
 #include <vector>
@@ -15,6 +16,9 @@ std::string to_string(std::vector<T> v);
 
 bool match(std::pair<int, int> dims, int nb_elems);
 
+// TEMPLATE FUNCTIONS
+
+// TODO: what is it, is it used somewhere?
 template <typename T>
 const T& clamp(const T& lo, const T& hi, const T& val);
 
@@ -22,12 +26,46 @@ const T& clamp(const T& lo, const T& hi, const T& val);
 // TODO(tobi) write tests for this function
 template <typename T>
 std::vector<std::vector<T>> columnmajor2matrix(std::vector<T> colvec, const size_t n,
-                                               const size_t m);
+                                               const size_t m)
+{
+    std::vector<std::vector<T>> matrix;
+    if (colvec.size() == 0) return matrix;
+
+    if (colvec.size() != n * m) throw std::length_error("colvec has inconsistent size");
+
+
+    matrix.reserve(n);
+    auto start = colvec.begin();
+    while (start != colvec.end())
+    {
+        matrix.push_back(std::vector<T>(start, start + m));
+        start = start + m + 1;  // element after end
+    }
+    if (matrix.size() != n)
+        throw std::length_error("conversion failed: output matrix as wrong dimensions");
+
+    return matrix;
+}
 
 /// conversion from standard c++ matrix representation to column-major representation
 // TODO(tobi) write tests for this function
 template <typename T>
-std::vector<T> matrix2columnmajor(std::vector<std::vector<T>> matrix);
+std::vector<T> matrix2columnmajor(std::vector<std::vector<T>> matrix)
+{
+    std::vector<T> colvec;
+    if (matrix.size() == 0) return colvec;
+
+    const size_t n = matrix.size();
+    const size_t m = matrix[0].size();
+    colvec.reserve(n * m);
+    for (auto const& row : matrix)
+    {
+        if (row.size() != m) throw std::length_error("matrix has inconsistent dimensions");
+        colvec.insert(colvec.end(), row.begin(), row.end());
+    }
+    return colvec;
+}
+
 }  // namespace acados
 
 #endif  // INTERFACES_ACADOS_CPP_UTILS_HPP_
